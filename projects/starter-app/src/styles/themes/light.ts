16c4bd49fee7aa784e5e4d1b33d1cf9ccab5eb8f
Abdou-Raouf ATARMLA <achilleatarmla@gmail.com>
2024-10-01 12:07:52 +0000

    feat: basic theming setup completed

---
+++ projects/starter-app/src/styles/themes/light.ts
@@ -0,0 +1,21 @@
+export const lightTheme = {
+    label: "Light",
+    value: "light",
+    properties: {
+      primary: 'hsl(198, 38%, 20%)',
+      secondary: 'hsl(191, 82%, 41%)',
+      accent: 'hsl(188, 55%, 71%)',
+      background: 'hsl(204, 45%, 98%)',
+      base: 'hsl(0, 1%, 21%)',
+      muted: 'hsl(0, 0%, 42%)',
+      'light-muted': 'hsl(0, 0%, 85%)',
+      corn: 'hsl(38, 87%, 70%)',
+      seal: 'hsl(179, 69%, 36%)',
+      ocean: 'hsl(210, 87%, 29%)',
+      emphasis: 'hsl(12, 86%, 55%)',
+      destructive: 'hsl(349, 85%, 44%)',
+      border: 'hsl(188, 55%, 71%)',
+      input: 'hsl(204, 45%, 98%)',
+      ring: 'hsl(191, 82%, 41%)',
+    }
+  };
--- projects/starter-app/tailwind.config.js
+++ projects/starter-app/tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+const { createThemes } = require('tw-colors');
+const { themes } = require('./src/styles/themes');
+const fontFamily = require('tailwindcss/defaultTheme').fontFamily;
+
 module.exports = {
   content: ["./projects/***/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ['var(--font-heading)', ...fontFamily.serif],
+        body: ['var(--font-body)', ...fontFamily.sans],
+      },
+      borderRadius: {
+        'none': 'var(--radius-none)',
+        'sm': 'var(--radius-sm)',
+        'md': 'var(--radius-md)',
+        'lg': 'var(--radius-lg)',
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [
+    require('tailwindcss-animate'),
+    createThemes(themes, {
+      produceCssVariable: (colorName) => `--rtwc-${colorName}`,
+      defaultTheme: 'light',
+    }),
+  ],
+};
